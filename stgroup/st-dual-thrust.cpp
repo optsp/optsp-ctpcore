b05dfc23ae5507c2ff05cbed58af6d73d57f6a1b
Xxiong <xxiong@cqu.edu.cn>
2019-01-10 21:55:56 +0800

    st: 添加dual thrust策略，作为策略demo

---
+++ stgroup/st-dual-thrust.cpp
@@ -0,0 +1,1 @@
+#include "st-dual-thrust.h"
---
+++ stgroup/st-dual-thrust.h
@@ -0,0 +1,19 @@
+#ifndef OPTSP_CTPCORE_ST_DUAL_THRUST_H_
+#define OPTSP_CTPCORE_ST_DUAL_THRUST_H_
+
+#include "stbase.hpp"
+
+
+class StDualThrust : public StBase {
+public:
+    ///获取策略的交易上下文
+    virtual StContext * GetStContext() {}
+    ///设置交易接口，直接调用接口的函数，即可实现下单/撤单等操作
+    virtual void SetTradeApi() {}
+    ///用于程序初始化时，向策略提供历史的Tick数据
+    virtual void InitTick() {}
+    ///向策略提供实时的Tick数据
+    virtual void PushTick() {}
+};
+
+#endif // !OPTSP_CTPCORE_ST_DUAL_THRUST_H_
