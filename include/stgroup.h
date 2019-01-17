c0e1aa88e0dec36ea2bee1f854f6a7b7ae0b6635
Xxiong <xxiong@cqu.edu.cn>
2019-01-17 22:13:26 +0800

    stgroup: 更新策略组

--- include/stgroup.h
+++ include/stgroup.h
@@ -2,18 +2,30 @@
 #define OPTSP_CTPCORE_STGROUP_H_
 
 #include <vector>
+#include <mutex>
+#include <boost/lockfree/queue.hpp>
+#include <condition_variable>
 #include "../include/platctp.h"
 #include "../include/config.h"
 #include "../include/logger.h"
 #include "../include/datatype.hpp"
+#include "../include/stbase.hpp"
+#include "../include/platstruct.hpp"
+#include "../include/tradestruct.hpp"
+#include "../include/quotestruct.hpp"
 
 
-static volatile void* cbfn = nullptr;
+static volatile void* stcbfn = nullptr;
 void PlatCallBack(int cbtype, void* ptr);
 
+std::mutex mutex_cb_buf;
+std::unique_lock<std::mutex> lock_cb_buf(mutex_cb_buf);
 
 class StGroup
 {
+private:   
+    std::condition_variable condi_var_cb_buf;
+
 private:
     Config * config = nullptr;
     Logger * logger = nullptr;
@@ -24,12 +36,13 @@ private:
     stgroup_callback_fn stgroup_callback = nullptr;
 
     std::vector<StBase*> *stContainer = nullptr;
-
+    boost::lockfree::queue<CbQuoteField> *cbQuoteQueue = nullptr;
+    boost::lockfree::queue<CbTradeField> *cbTradeQueue = nullptr;
 
 public:
     StGroup(Config *config, Logger *logger, quote_callback_fn qfn, trade_callback_fn tfn, stgroup_callback_fn sfn);
-    void DispatchSpi(int cbtype, void* ptr);
-
+    void SubmitSpi(int cbtype, void* ptr);
+    void DispatchSpi(); ///new thread for dispatch packages from plat.
 };
 
 #endif // !OPTSP_CTPCORE_STGROUP_H_
--- stgroup/stgroup.cpp
+++ stgroup/stgroup.cpp
@@ -1,4 +1,7 @@
 #include "../include/stgroup.h"
+#include <chrono>
+
+#include "st-server.h"
 #include "st-hans123.h"
 #include "st-dual-thrust.h"
 
@@ -6,29 +9,36 @@
 StGroup::StGroup(Config *config, Logger *logger, quote_callback_fn qfn, trade_callback_fn tfn, stgroup_callback_fn sfn)
     : config(config), logger(logger), quote_callback(qfn), trade_callback(tfn), stgroup_callback(sfn)
 {
-    cbfn = this;
+    stcbfn = this;
     std::stringstream log;
 
     plat = new PlatCtp(config, logger, PlatCallBack);
-    stContainer = new std::vector<StBase*>();
 
-    StBase *st1 = new StHans123();
-    StDualThrust *st2 = new StDualThrust();
+    stContainer = new std::vector<StBase*>();
+    cbQuoteQueue = new boost::lockfree::queue<CbQuoteField>(1024);
+    cbTradeQueue = new boost::lockfree::queue<CbTradeField>(1024);
+
+
+    StBase *st0 = new StServer();           ///用于通过socket与其他平台实现的策略进行交互
+    StBase *st1 = new StHans123();          ///demo1
+    StBase *st2 = new StDualThrust();       ///demo2
+    stContainer->push_back(st0);
     stContainer->push_back(st1);
     stContainer->push_back(st2);
 
-    for (auto st : *stContainer) {
-        st->GetStContext();
-    }
 }
 
 
-void StGroup::DispatchSpi(int cbtype, void* ptr)
+void StGroup::SubmitSpi(int cbtype, void* ptr)
 {
     if (cbtype > CB_QUOTE_TYPE_BEGIN && cbtype < CB_QUOTE_TYPE_END) {
+        cbQuoteQueue->push(*((CbQuoteField*) ptr));
+        condi_var_cb_buf.notify_all();
         (*quote_callback)(cbtype, ptr);
     }
     else if (cbtype > CB_TRADE_TYPE_BEGIN && cbtype < CB_TRADE_TYPE_END) {
+        cbTradeQueue->push(*((CbTradeField*) ptr));
+        condi_var_cb_buf.notify_all();
         (*trade_callback)(cbtype, ptr);
     }
     else if (cbtype > CMD_PLAT_QUOTE_TYPE_BEGIN && cbtype < CMD_PLAT_QUOTE_TYPE_END) {
@@ -43,7 +53,34 @@ void StGroup::DispatchSpi(int cbtype, void* ptr)
 }
 
 
+void StGroup::DispatchSpi()
+{
+    CbQuoteField cbQuote;
+    CbTradeField cbTrade;
+    /* 优先调度行情回调；在行情回调队列为空时再调度一个交易回调 */
+    /* 两个队列都为空时，阻塞等待唤醒或等待一毫秒的timeout */
+    while (true) {
+        condi_var_cb_buf.wait_for(lock_cb_buf, std::chrono::milliseconds(1));
+        while (!cbQuoteQueue->empty()) {
+            do {
+                //TODO: 将quoteQueue清空
+                cbQuoteQueue->pop(cbQuote);
+
+
+            } while (!cbQuoteQueue->empty());
+
+            do {
+                //TODO: 在quoteQueue为空时对tradeQueue进行一次调度
+                cbTradeQueue->pop(cbTrade);
+
+
+            } while (cbQuoteQueue->empty());
+        }
+    }
+}
+
+
 void PlatCallBack(int cbtype, void* ptr) {
-    StGroup *stgroup = (StGroup*)cbfn;
-    stgroup->DispatchSpi(cbtype, ptr);
+    StGroup *stgroup = (StGroup*)stcbfn;
+    stgroup->SubmitSpi(cbtype, ptr);
 }
